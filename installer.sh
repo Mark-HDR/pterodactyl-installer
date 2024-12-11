2e028766cf88476fe0511f7ec418255c60df14b3
Official Mark-HDR <128824042+Mark-HDR@users.noreply.github.com>
2024-12-11 13:00:22 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/usr/bin/env bash
 # tes
 dist="$(. /etc/os-release && echo "$ID")"
 version="$(. /etc/os-release && echo "$VERSION_ID")"
@@ -14,19 +13,33 @@ finish(){
 panel_conf(){
     [ "$SSL" == true ] && appurl="https://$FQDN"
     [ "$SSL" == false ] && appurl="http://$FQDN"
-    DBPASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1`
-    mariadb -u root -e "CREATE USER 'pterodactyl'@'127.0.0.1' IDENTIFIED BY '$DBPASSWORD';" && mariadb -u root -e "CREATE DATABASE panel;" && mariadb -u root -e "GRANT ALL PRIVILEGES ON panel.* TO 'pterodactyl'@'127.0.0.1' WITH GRANT OPTION;" && mariadb -u root -e "FLUSH PRIVILEGES;"
+    DBPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+    mariadb -u root -e "CREATE USER 'pterodactyl'@'127.0.0.1' IDENTIFIED BY '$DBPASSWORD';"
+    mariadb -u root -e "CREATE DATABASE panel;"
+    mariadb -u root -e "GRANT ALL PRIVILEGES ON panel.* TO 'pterodactyl'@'127.0.0.1' WITH GRANT OPTION;"
+    mariadb -u root -e "FLUSH PRIVILEGES;"
+    
     php artisan p:environment:setup --author="$EMAIL" --url="$appurl" --timezone="CET" --telemetry=false --cache="redis" --session="redis" --queue="redis" --redis-host="localhost" --redis-pass="null" --redis-port="6379" --settings-ui=true
     php artisan p:environment:database --host="127.0.0.1" --port="3306" --database="panel" --username="pterodactyl" --password="$DBPASSWORD"
     php artisan migrate --seed --force
     php artisan p:user:make --email="$EMAIL" --username="$USERNAME" --name-first="$FIRSTNAME" --name-last="$LASTNAME" --password="$PASSWORD" --admin=1
     chown -R www-data:www-data /var/www/pterodactyl/*
     curl -o /etc/systemd/system/pteroq.service https://raw.githubusercontent.com/amiruldev20/ippanel/main/configs/pteroq.service
-    (crontab -l ; echo "* * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1")| crontab -
+    (crontab -l ; echo "* * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1") | crontab -
     sudo systemctl enable --now redis-server
     sudo systemctl enable --now pteroq.service
-    [ "$WINGS" == true ] && curl -sSL https://get.docker.com/ | CHANNEL=stable bash && systemctl enable --now docker && mkdir -p /etc/pterodactyl && apt-get -y install curl tar unzip && curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")" && curl -o /etc/systemd/system/wings.service https://raw.githubusercontent.com/amiruldev20/ippanel/main/configs/wings.service && chmod u+x /usr/local/bin/wings
-    if  [ "$SSL" =  "true" ]; then
+    
+    if [ "$WINGS" == true ]; then
+        curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+        systemctl enable --now docker
+        mkdir -p /etc/pterodactyl
+        apt-get -y install curl tar unzip
+        curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+        curl -o /etc/systemd/system/wings.service https://raw.githubusercontent.com/amiruldev20/ippanel/main/configs/wings.service
+        chmod u+x /usr/local/bin/wings
+    fi
+    
+    if [ "$SSL" = "true" ]; then
         rm -rf /etc/nginx/sites-enabled/default
         curl -o /etc/nginx/sites-enabled/pterodactyl.conf https://raw.githubusercontent.com/amiruldev20/ippanel/main/configs/pterodactyl-nginx-ssl.conf
         sed -i -e "s@<domain>@${FQDN}@g" /etc/nginx/sites-enabled/pterodactyl.conf
@@ -35,9 +48,10 @@ panel_conf(){
         systemctl start nginx
         finish
     fi
-    if  [ "$SSL" =  "false" ]; then
+    
+    if [ "$SSL" = "false" ]; then
         rm -rf /etc/nginx/sites-enabled/default
-        curl -o /etc/nginx/sites-enabled/pterodactyl.conf https://raw.githubusercontent.com/amiruldev20/ippanel/main/configs/pterodactyl-nginx.conf
+        curl -o / etc/nginx/sites-enabled/pterodactyl.conf https://raw.githubusercontent.com/amiruldev20/ippanel/main/configs/pterodactyl-nginx.conf
         sed -i -e "s@<domain>@${FQDN}@g" /etc/nginx/sites-enabled/pterodactyl.conf
         systemctl restart nginx
         finish
@@ -48,7 +62,7 @@ panel_install(){
     echo "" 
     apt update
     apt install certbot -y
-    if  [ "$dist " = "ubuntu" ] && [ "$version" = "20.04" ]; then
+    if [ "$dist" = "ubuntu" ] && [ "$version" = "20.04" ]; then
         apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
         LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
         curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor --batch --yes -o /usr/share/keyrings/redis-archive-keyring.gpg
@@ -69,7 +83,7 @@ panel_install(){
     if [ "$dist" = "debian" ] && [ "$version" = "11" ]; then
         apt -y install software-properties-common curl ca-certificates gnupg2 sudo lsb-release
         echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
-        curl -fsSL  https://packages.sury.org/php/apt.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/sury-keyring.gpg
+        curl -fsSL https://packages.sury.org/php/apt.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/sury-keyring.gpg
         curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
         echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
         apt update -y
@@ -83,38 +97,34 @@ panel_install(){
         curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
         echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
         apt update -y
-        curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+        curl -sS https ://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
     fi
     apt install -y mariadb-server tar unzip git redis-server
     sed -i 's/character-set-collations = utf8mb4=uca1400_ai_ci/character-set-collations = utf8mb4=utf8mb4_general_ci/' /etc/mysql/mariadb.conf.d/50-server.cnf
     systemctl restart mariadb
-    apt -y install php8 .1 php8.1-{cli,gd,mysql,pdo,mbstring,tokenizer,bcmath,xml,fpm,curl,zip}
+    apt -y install php8.1 php8.1-{cli,gd,mysql,pdo,mbstring,tokenizer,bcmath,xml,fpm,curl,zip}
     curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
     pause 0.5s
-    mkdir /var
-    mkdir /var/www
-    mkdir /var/www/pterodactyl
+    mkdir -p /var/www/pterodactyl
     cd /var/www/pterodactyl
     curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/download/v1.11.7/panel.tar.gz
     tar -xzvf panel.tar.gz
     chmod -R 755 storage/* bootstrap/cache/
     cp .env.example .env
-    command composer install --no-dev --optimize-autoloader --no-interaction
+    composer install --no-dev --optimize-autoloader --no-interaction
     php artisan key:generate --force
     apt install nginx -y
     panel_conf
 }
 
-#! /bin/sh -
-
-FQDN=`echo $1`
-SSL=`echo $2`
-EMAIL=`echo $3`
-USERNAME=`echo $4`
-FIRSTNAME=`echo $5`
-LASTNAME=`echo $6`
-PASSWORD=`echo $7`
-WINGS=`echo $8`
+FQDN=$(echo $1)
+SSL=$(echo $2)
+EMAIL=$(echo $3)
+USERNAME=$(echo $4)
+FIRSTNAME=$(echo $5)
+LASTNAME=$(echo $6)
+PASSWORD=$(echo $7)
+WINGS=$(echo $8)
 
 if [ -z "$FQDN" ] || [ -z "$SSL" ] || [ -z "$EMAIL" ] || [ -z "$USERNAME" ] || [ -z "$FIRSTNAME" ] || [ -z "$LASTNAME" ] || [ -z "$PASSWORD" ] || [ -z "$WINGS" ]; then
     echo "Error! The usage of this script is incorrect."
